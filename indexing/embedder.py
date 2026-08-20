3976fb81f4c47fe5990a75754326878ca23abea3
Mohamed Sameh <majormody1@gmail.com>
2026-08-20 19:11:38 +0300

    Finished Implementing indexing/embedder.py

--- indexing/embedder.py
+++ indexing/embedder.py
@@ -1,6 +1,6 @@
 import config
 from sentence_transformers import SentenceTransformer
-import numpy
+
 class Embedder:
     def __init__(self, model_name: str = config.EMBED_MODEL_NAME):
 
@@ -8,10 +8,10 @@ class Embedder:
 
     def embed_texts(self, texts: list[str]) -> list[list[float]]:
         
-        embeddings = self._model.encode(texts)
+        embeddings = self._model.encode(texts, show_progress_bar=False)
         return embeddings.tolist()
 
     def embed_query(self, query: str) -> list[float]: 
 
-        return self._model.encode_query(query).tolist()
+        return self._model.encode([query], show_progress_bar=False)[0].tolist()
 
