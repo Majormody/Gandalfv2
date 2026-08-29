574081f8f0cf3a07f1033f77f5636566d6382942
Mohamed Sameh <majormody1@gmail.com>
2026-08-29 17:27:18 +0300

    Using load_env to load secrets.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 EMBED_MODEL_NAME = "all-MiniLM-L6-v2"
 GROQ_API_KEY = os.environ["GROQ_API_KEY"]
-GROQ_MODEL_NAME = "llama-3.1-8b-instant"
+GROQ_MODEL_NAME = "openai/gpt-oss-20b"
